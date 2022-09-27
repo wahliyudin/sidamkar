211aa37ef6860e43b1fb9d7a3fae07cb433205de
wahliyudin <wahliyudinzein@gmail.com>
2022-09-27 17:11:50 +0700

    Revisi login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,51 +4,60 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login - Mazer Admin Dashboard</title>
+    <title>Login</title>
     <link rel="stylesheet" href="{{ asset('assets/css/pages/auth.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/main/app.css') }}">
     <link rel="shortcut icon" href="{{ asset('assets/images/logo/favicon.svg') }}" type="image/x-icon">
     <link rel="shortcut icon" href="{{ asset('assets/images/logo/favicon.png') }}" type="image/png">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 </head>
 
 <body>
     <div id="auth">
-
         <div class="row h-100">
-            <div class="col-lg-7 d-none d-lg-block">
+            <div class="col-lg-6 d-none d-lg-block">
                 <div id="auth-right">
-                    <h1 class="text-white login-title">Sistem Informasi Jabatan Fungsional DAMKAR</h1>
+                    <h1 data-aos="fade-right" data-aos-duration="2000" class="text-white login-title">Sistem Informasi
+                        Jabatan Fungsional DAMKAR
+                    </h1>
                 </div>
             </div>
-            <div class="col-lg-5 col-12">
+            <div class="col-lg-6 col-12">
                 <div id="auth-left">
                     <div class="auth-logo">
                         <a href="index.html">
                             <img src="{{ asset('assets/images/template/logo.png') }}" alt="Logo">
                         </a>
                     </div>
-                    <form action="index.html">
-                        <div class="form-group position-relative has-icon-left mb-4">
-                            <input type="text" class="form-control form-control-xl" placeholder="Username"
-                                autocomplete="none">
-                            <div class="form-control-icon">
-                                <i class="bi bi-person"></i>
+                    <div class="form">
+                        <form action="index.html">
+                            <div class="form-group position-relative has-icon-left mb-4">
+                                <input type="text" class="form-control form-control-xl" placeholder="Username"
+                                    autocomplete="none">
+                                <div class="form-control-icon">
+                                    <i class="bi bi-person"></i>
+                                </div>
                             </div>
-                        </div>
-                        <div class="form-group position-relative has-icon-left mb-4">
-                            <input type="password" class="form-control form-control-xl" placeholder="Password">
-                            <div class="form-control-icon">
-                                <i class="bi bi-shield-lock"></i>
+                            <div class="form-group position-relative has-icon-left mb-4">
+                                <input type="password" class="form-control form-control-xl" placeholder="Password">
+                                <div class="form-control-icon">
+                                    <i class="bi bi-shield-lock"></i>
+                                </div>
                             </div>
-                        </div>
-                        <div class="form-check form-check-lg d-flex align-items-end">
-                            <input class="form-check-input me-2" type="checkbox" value="" id="flexCheckDefault">
-                            <label class="form-check-label text-gray-600" for="flexCheckDefault">
-                                Keep me logged in
-                            </label>
-                        </div>
-                        <button class="btn btn-primary btn-block btn-lg mt-5">Log in</button>
-                    </form>
+                            <div class="form-check form-check-lg d-flex align-items-end">
+                                <input class="form-check-input me-2" type="checkbox" value=""
+                                    id="flexCheckDefault">
+                                <label class="form-check-label text-gray-600" for="flexCheckDefault">
+                                    Remamber Me
+                                </label>
+                            </div>
+                            <button class="btn btn-primary btn-block btn-lg mt-3">Log in</button>
+                            <div class="d-flex flex-column mt-2">
+                                <a style="font-size: 14px;" href="">Register Admin Provinsi, Kab/Kota</a>
+                                <a style="font-size: 14px;" href="">Register Aparatur</a>
+                            </div>
+                        </form>
+                    </div>
                     {{-- <div class="text-center mt-5 text-lg fs-4">
                         <p class="text-gray-600">Don't have an account? <a href="auth-register.html"
                                 class="font-bold">Sign
@@ -64,6 +73,11 @@
                 alt="">
         </div>
     </div>
+
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script>
+        AOS.init();
+    </script>
 </body>
 
 </html>
