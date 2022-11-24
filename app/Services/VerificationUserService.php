4b6a6e9a85a67f0305fe00f833ff99084c6d98a0
wahliyudin <wahliyudinzein@gmail.com>
2022-11-24 17:17:20 +0700

    Memberikan role saat verifikasi struktural di kabkota

--- app/Services/VerificationUserService.php
+++ app/Services/VerificationUserService.php
@@ -2,13 +2,18 @@
 
 namespace App\Services;
 
+use App\Models\User;
 use App\Notifications\UserReject;
 use App\Notifications\UserVerified;
 use App\Repositories\UserRepository;
+use App\Traits\AuthTrait;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class VerificationUserService
 {
+    use AuthTrait;
+
     protected UserRepository $userRepository;
 
     public function __construct(UserRepository $userRepository)
@@ -16,9 +21,25 @@ class VerificationUserService
         $this->userRepository = $userRepository;
     }
 
-    public function verification($id)
+    public function verification(Request $request, $id)
     {
         $user = $this->userRepository->getUserById($id);
+        $atasan = null;
+        $penilaiPenetap = [];
+        foreach ($request->jabatans as $jabatan) {
+            if ($jabatan == 'atasan_langsung') $atasan = $jabatan;
+            if (in_array($jabatan, ['penilai_ak', 'penetap_ak'])) array_push($penilaiPenetap, $jabatan);
+        }
+        if (count($penilaiPenetap) > 0) {
+            $users = User::query()->with(['userPejabatStruktural' => function($query){
+                $query->where('kab_kota_id', $this->authUser()->load('userProvKabKota')?->userProvKabKota->kab_kota_id);
+            }])->whereRoleIs(['penilai_ak', 'penetap_ak'])->get();
+            if (count($users) > 0) {
+                throw ValidationException::withMessages(['message' => 'Maaf Kabupaten Kota sudah mempunyai penilai ak dan penetap ak']);
+            }
+        }
+        array_push($penilaiPenetap, $atasan);
+        $user->attachRoles($penilaiPenetap);
         $this->userRepository->updateStatusAkunVerified($user);
         $user->notify(new UserVerified());
     }
