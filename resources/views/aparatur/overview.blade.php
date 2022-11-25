1776e51a4f0b667e413b2bf9c0bc87c519b1c60a
FadilaPrasetyo8 <fadilaprasetyo8@gmail.com>
2022-11-25 14:16:31 +0700

    responsive dashboard aparatur

--- resources/views/aparatur/overview.blade.php
+++ resources/views/aparatur/overview.blade.php
@@ -16,10 +16,11 @@
                                 <i class="fa-solid fa-circle-exclamation"></i>
                             </div>
                             <div class="d-flex flex-column ms-2">
-                                <p style="margin: 0 !important; color: #809FB8; font-family: 'Roboto'; font-size: 14px;">
+                                <p style="margin: 0 !important; color: #809FB8; font-family: 'Roboto'; font-size: 14px;"
+                                    class="target-h1">
                                     Angka Kredit Sebelumnya
                                 </p>
-                                <h2 style="font-family: 'Roboto';color: #06152B;" class="target">44</h2>
+                                <h2 style="font-family: 'Roboto';color: #06152B;" class="target target-h2">44</h2>
                             </div>
                         </div>
                     </div>
@@ -33,10 +34,11 @@
                                 <i class="fa-solid fa-bullseye"></i>
                             </div>
                             <div class="d-flex flex-column ms-2">
-                                <p style="margin: 0 !important; color: #809FB8; font-family: 'Roboto'; font-size: 14px;">
+                                <p style="margin: 0 !important; color: #809FB8; font-family: 'Roboto'; font-size: 14px;"
+                                    class="target-h1">
                                     Target Angka Kredit
                                 </p>
-                                <h2 style="font-family: 'Roboto';color: #06152B;" class="target">100</h2>
+                                <h2 style="font-family: 'Roboto';color: #06152B;" class="target target-h2">100</h2>
                             </div>
                         </div>
                     </div>
@@ -50,10 +52,11 @@
                                 <i class="fa-solid fa-sliders"></i>
                             </div>
                             <div class="d-flex flex-column ms-2">
-                                <p style="margin: 0 !important; color: #809FB8; font-family: 'Roboto'; font-size: 14px;">
+                                <p style="margin: 0 !important; color: #809FB8; font-family: 'Roboto'; font-size: 14px;"
+                                    class="target-h1">
                                     Capaian Angka Kredit
                                 </p>
-                                <h2 style="font-family: 'Roboto';color: #06152B;" class="target">56</h2>
+                                <h2 style="font-family: 'Roboto';color: #06152B;" class="target target-h2">56</h2>
                             </div>
                         </div>
                     </div>
@@ -67,7 +70,8 @@
                                 <i class="fa-solid fa-calendar-days"></i>
                             </div>
                             <div class="d-flex flex-column ms-2">
-                                <p style="margin: 0 !important; color: #809FB8; font-family: 'Roboto'; font-size: 14px;">
+                                <p style="margin: 0 !important; color: #809FB8; font-family: 'Roboto'; font-size: 14px;"
+                                    class="target-h1">
                                     Periode Aktif
                                 </p>
                                 <h2 style="font-family: 'Roboto';color: #06152B; font-size: 14px;"
@@ -195,6 +199,17 @@
             .target-periode {
                 font-size: 14px !important;
             }
+        }
+
+        @media screen and (min-width: 750px) and (max-width: 1000px) {
+            .target-h2 {
+                font-size: 14px;
+            }
+
+            .target-periode {
+                font-size: 11px !important;
+            }
+
         }
     </style>
 @endsection
