7baa5602ce45e9abd9165d8d9c368faa033eab7b
unknown <coderown08@gmail.com>
2022-12-08 12:26:23 +0700

    input skp

---
+++ app/Models/KetentuanSkpFungsional.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class KetentuanSkpFungsional extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'ketentuan_skp_id',
+        'status',
+        'user_id',
+        'nilai_skp',
+        'file'
+    ];
+
+    public function ketentuanSkp()
+    {
+        return $this->belongsTo(KetentuanSkp::class);
+    }
+}
