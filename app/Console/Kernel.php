88751767a70e49eec63923206ee63ddf24c8bc04
Wahliyudin <wahliyudinzein@gmail.com>
2022-12-25 20:58:05 +0700

    Coba cron job

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -9,6 +9,7 @@ class Kernel extends ConsoleKernel
 {
     protected $commands = [
         Commands\CleanTemporaryDaily::class,
+        Commands\TransformPeriode::class,
     ];
     /**
      * Define the application's command schedule.
