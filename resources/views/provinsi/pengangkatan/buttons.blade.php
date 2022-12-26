f525160003d6f9f4bf4497f448c3337716feb5ad
unknown <coderown08@gmail.com>
2022-12-26 14:47:38 +0700

    minor

--- resources/views/provinsi/pengangkatan/buttons.blade.php
+++ resources/views/provinsi/pengangkatan/buttons.blade.php
@@ -1,9 +1,9 @@
 <div class="d-flex align-items-cen">
-    <button {{ $row->is_naik == 0 ? 'disabled' : '' }} data-penetapan="{{ $row->id }}"
+    <button {{ $row->is_naik != 0 ? 'disabled' : '' }} data-penetapan="{{ $row->id }}"
         class="btn btn-dark-reverse me-2 tolak">
         <i class="fas fa-xmark"></i>
     </button>
-    <button {{ $row->is_naik == 0 ? 'disabled' : '' }} class="btn btn-green-reverse me-2" data-bs-toggle="modal"
+    <button {{ $row->is_naik != 0 ? 'disabled' : '' }} class="btn btn-green-reverse me-2" data-bs-toggle="modal"
         data-bs-target="#pengangkatan{{ $row->id }}">
         <i class="fas fa-check"></i>
     </button>
