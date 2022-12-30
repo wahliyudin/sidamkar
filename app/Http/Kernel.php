14d21b8e38e5a29080a23b3a6904361750d7a33a
unknown <coderown08@gmail.com>
2022-12-30 15:28:10 +0700

    testing https

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -39,7 +39,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \App\Http\Middleware\IsVerified::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Http\Middleware\HttpsProtocol::class
+            // \App\Http\Middleware\HttpsProtocol::class
         ],
 
         'api' => [
