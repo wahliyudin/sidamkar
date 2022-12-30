191d9112a70fd1f68bfd327856aaca45831c3c94
unknown <coderown08@gmail.com>
2022-12-30 20:37:38 +0700

    fix table

--- app/DataTables/Provinsi/MenteDataTable.php
+++ app/DataTables/Provinsi/MenteDataTable.php
@@ -118,7 +118,7 @@ class MenteDataTable extends DataTable
             ->responsive('true')
             ->orderCellsTop()
             ->columns($this->getColumns())
-            ->minifiedAjax()
+            ->minifiedAjax(env('APP_URL') . '/provinsi/data-mente')
             ->dom('lfrtip')
             ->orderBy(1)
             ->buttons(
