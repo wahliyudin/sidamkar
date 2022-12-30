323cc1200cb08328304be084ce8c6d18e8e547d6
unknown <coderown08@gmail.com>
2022-12-30 16:06:47 +0700

    testing https

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -17,10 +17,9 @@ class HttpsProtocol
      */
     public function handle(Request $request, Closure $next)
     {
-        if (!$request->secure() && app()->environment('production')) {
+        if (!$request->secure()) {
             return redirect()->secure($request->getRequestUri());
         }
-
         return $next($request);
     }
 }
