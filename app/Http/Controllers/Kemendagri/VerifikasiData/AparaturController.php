51b2d9752cca77bee463107b6bae2c85faecf400
unknown <coderown08@gmail.com>
2023-01-02 20:11:54 +0700

    fix table minor

--- app/Http/Controllers/Kemendagri/VerifikasiData/AparaturController.php
+++ app/Http/Controllers/Kemendagri/VerifikasiData/AparaturController.php
@@ -30,19 +30,19 @@ class AparaturController extends Controller
     {
         if ($request->ajax()) {
             $data = DB::select('SELECT
-                user_aparaturs.nama,
-                user_aparaturs.nip,
-                provinsis.nama AS provinsi,
-                kab_kotas.nama AS kab_kota,
-                roles.display_name AS jabatan,
-                pangkat_golongan_tmts.nama AS pangkat
+            user_aparaturs.nama,
+            user_aparaturs.nip,
+            provinsis.nama AS provinsi,
+            kab_kotas.nama AS kab_kota,
+            roles.display_name AS jabatan,
+            pangkat_golongan_tmts.nama AS pangkat
             FROM users
             JOIN user_aparaturs ON user_aparaturs.user_id = users.id
             LEFT JOIN provinsis ON user_aparaturs.provinsi_id = provinsis.id
             LEFT JOIN kab_kotas ON kab_kotas.id = user_aparaturs.kab_kota_id
-            JOIN role_user ON role_user.user_id = users.id
-            JOIN roles ON roles.id = role_user.role_id
-            LEFT JOIN pangkat_golongan_tmts ON user_aparaturs.pangkat_golongan_tmt_id');
+            LEFT JOIN role_user ON role_user.user_id = users.id
+            LEFT JOIN roles ON roles.id = role_user.role_id
+            LEFT JOIN pangkat_golongan_tmts ON pangkat_golongan_tmts.id = user_aparaturs.pangkat_golongan_tmt_id');
             return DataTables::of($data)
                 ->addIndexColumn()
                 ->make(true);
