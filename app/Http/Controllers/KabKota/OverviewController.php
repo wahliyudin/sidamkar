f5ff1a0d2775e56c3a235a984c0f75bf34cf2b8f
root <root@ip-10-0-165-219.ap-southeast-3.compute.internal>
2023-01-13 07:44:39 +0000

    revisi overview kabkot from server

--- app/Http/Controllers/KabKota/OverviewController.php
+++ app/Http/Controllers/KabKota/OverviewController.php
@@ -18,11 +18,11 @@ class OverviewController extends Controller
         $user = DB::table('users')->join('user_prov_kab_kotas', 'user_prov_kab_kotas.user_id', '=', 'users.id')->where('users.id', Auth::user()->id)->get();
         $kab_kota_id = $user[0]->kab_kota_id;
         $total_fungsional = DB::table('user_aparaturs')->where('user_aparaturs.tingkat_aparatur', '=', 'kab_kota')->where([['user_aparaturs.kab_kota_id', $kab_kota_id]])->count();
-        $total_struktural = DB::table('user_pejabat_strukturals')->where('user_pejabat_strukturals.tingkat_aparatur', '=', 'kab_kota')->where([['user_pejabat_strukturals.kab_kota_id', $kab_kota_id]])->count();
+        $total_struktural = DB::table('users')->rightJoin('user_pejabat_strukturals', 'user_pejabat_strukturals.user_id', '=', 'users.id')->join('role_user', 'role_user.user_id', '=', 'users.id')->where('user_pejabat_strukturals.tingkat_aparatur', '=', 'kab_kota')->whereRaw('role_user.role_id > 10 and role_user.role_id < 16')->where([['user_pejabat_strukturals.kab_kota_id', $kab_kota_id]])->groupBy('users.id')->get()->count();
 
         $total_damkar = DB::table('users')->rightJoin('user_aparaturs', 'user_aparaturs.user_id', '=', 'users.id')->join('role_user', 'role_user.user_id', '=', 'users.id')->where('role_user.role_id', '<', 5)->where('user_aparaturs.tingkat_aparatur', '=', 'kab_kota')->where([['user_aparaturs.kab_kota_id', $kab_kota_id]])->count();
 
-        $total_analis = DB::table('users')->rightJoin('user_aparaturs', 'user_aparaturs.user_id', '=', 'users.id')->join('role_user', 'role_user.user_id', '=', 'users.id')->whereRaw('role_user.user_id > 4 and role_user.user_id < 8')->where('user_aparaturs.tingkat_aparatur', '=', 'kab_kota')->where([['user_aparaturs.kab_kota_id', $kab_kota_id]])->count();
+        $total_analis = DB::table('users')->rightJoin('user_aparaturs', 'user_aparaturs.user_id', '=', 'users.id')->join('role_user', 'role_user.user_id', '=', 'users.id')->whereRaw('role_user.role_id > 4 and role_user.role_id < 8')->where('user_aparaturs.tingkat_aparatur', '=', 'kab_kota')->where([['user_aparaturs.kab_kota_id', $kab_kota_id]])->groupBy('users.id')->get()->count();
 
         $total = [
             'fungsional' => $total_fungsional,
